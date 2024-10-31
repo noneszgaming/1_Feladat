7601caf5d956863de1d660814a9001e4299e7a8c
Szilagyi Zoltan <zoltan.szilagyi@devconsult.hu>
2024-10-31 10:53:43 +0100

    1. feladat Szilagyi Zoltan

--- 1_Feladat/Program.cs
+++ 1_Feladat/Program.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 
-
 class Program
 {
     static void Main()
     {
-
+        // Bekérjük a karaktert és a sztringet a felhasználótól
         Console.Write("Adj meg egy karaktert: ");
         char karakter = Console.ReadKey().KeyChar;
-        Console.WriteLine();
+        Console.WriteLine(); // Új sor a karakter beírása után
 
-        Console.Write("Adj meg egy szoveg: ");
-        string szoveg = Console.ReadLine();
+        Console.Write("Adj meg egy sztringet: ");
+        string sztring = Console.ReadLine();
 
+        // Megkeressük a karaktert a sztringben
+        int pozíció = sztring.IndexOf(karakter);
 
-        int pos = szoveg.IndexOf(karakter);
-
-
-        if (pos != -1)
+        // Kiírjuk az eredményt
+        if (pozíció != -1)
         {
-            Console.WriteLine($"A karakter pozicioja: {pos}");
+            Console.WriteLine($"A karakter pozíciója: {pozíció}");
         }
         else
         {
-            Console.WriteLine("A karakter nem talalhato a szovegben.");
+            Console.WriteLine("A karakter nem található a sztringben.");
         }
     }
 }
