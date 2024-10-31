e55beeca7a27cc0053f6bdc22f6ca419642fe568
Szilagyi Zoltan <zoltan.szilagyi@devconsult.hu>
2024-10-31 10:49:31 +0100

    Add project files.

---
+++ 1_Feladat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Adj meg egy karaktert: ");
+        char karakter = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Adj meg egy szoveg: ");
+        string szoveg = Console.ReadLine();
+
+
+        int pos = szoveg.IndexOf(karakter);
+
+
+        if (pos != -1)
+        {
+            Console.WriteLine($"A karakter pozicioja: {pos}");
+        }
+        else
+        {
+            Console.WriteLine("A karakter nem talalhato a szovegben.");
+        }
+    }
+}
